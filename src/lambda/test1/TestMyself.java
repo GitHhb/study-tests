7119080c23d9a91e671ad535ad7ac359d3afdea3
Leandro <githhb@mittere.net>
2016-11-02 01:23:27 +0100

    update TestMyself.java

--- src/lambda/test1/TestMyself.java
+++ src/lambda/test1/TestMyself.java
@@ -5,11 +5,18 @@ import java.util.function.BiFunction;
 public class TestMyself {
 
 	public static void main(String[] args) {
+		
 		BiFunction<Integer, Integer, Integer> f = (x, y) -> x + y;
 		System.out.println("Sum: " + mathOp(2, 3, f));
+
+		BiFunction<String, String, String> c = (x, y) -> x + y;
+		System.out.println("Concat: " + mathOp("Hal", "lo", c));
+
+		BiFunction<Integer, Integer, String> a = (x, y) -> {return x.toString() + y.toString();};
+		System.out.println("Concat: " + mathOp(3, 5, a));
 	}
 	
-	static <T> T mathOp(T a, T b, BiFunction<T, T, T> op) {
+	static <R, S, T> T mathOp(R a, S b, BiFunction<R, S, T> op) {
 		return op.apply(a, b);
 		
 	}
